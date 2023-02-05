a36ac7a28a00a79054a43af44977b79bd652f3eb
Hector Sosa <hello@hectorsosa.me>
2023-02-05 22:26:33 +0100

    Updating Server Routes

--- src/server/routers/user.ts
+++ src/server/routers/user.ts
@@ -1,38 +1,35 @@
 import { t } from "../trpc";
-import { z } from "zod";
-import sanity from "../../lib/sanity-client";
 import { reviewSchema } from "../../components/UI/ReviewModal";
 import { contactSchema } from "../../components/Home/Contact";
+import sanity from "@/sanity/client";
 
 export const userRouter = t.router({
-    sendReview: t.procedure.input(reviewSchema).mutation(async ({ input }) => {
-        const document = {
-            ...input,
-            _type: "reviews",
-            read: false,
-            date: new Date().toISOString(),
-        };
-        try {
-            await sanity.create(document);
-            return;
-        } catch (_) {
-            return;
-        }
-    }),
-    sendContact: t.procedure
-        .input(contactSchema)
-        .mutation(async ({ input }) => {
-            const document = {
-                ...input,
-                _type: "messages",
-                read: false,
-                date: new Date().toISOString(),
-            };
-            try {
-                await sanity.create(document);
-                return;
-            } catch (_) {
-                return;
-            }
-        }),
+  sendReview: t.procedure.input(reviewSchema).mutation(async ({ input }) => {
+    const document = {
+      ...input,
+      _type: "reviews",
+      read: false,
+      date: new Date().toISOString(),
+    };
+    try {
+      await sanity.create(document);
+      return;
+    } catch (_) {
+      return;
+    }
+  }),
+  sendContact: t.procedure.input(contactSchema).mutation(async ({ input }) => {
+    const document = {
+      ...input,
+      _type: "messages",
+      read: false,
+      date: new Date().toISOString(),
+    };
+    try {
+      await sanity.create(document);
+      return;
+    } catch (_) {
+      return;
+    }
+  }),
 });
