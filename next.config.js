da362d34d70e15b91a4b3c777df9ae83a93b707c
Hector Sosa <hello@hectorsosa.me>
2023-03-23 15:01:05 +0100

    Updating Analytics

--- next.config.js
+++ next.config.js
@@ -5,14 +5,6 @@ const nextConfig = {
     locales: ["en"],
     defaultLocale: "en",
   },
-  async rewrites() {
-    return [
-      {
-        source: "/js/script.js",
-        destination: "https://plausible.io/js/script.js",
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
